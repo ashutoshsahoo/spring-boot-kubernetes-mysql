3ba32e83de78f1c3577f0c1e0b22a6595d77da78
ashutoshsahoo <15648046+ashutoshsahoo@users.noreply.github.com>
2020-05-24 18:01:27 +0530

    updated code

--- src/main/java/com/ashu/demo/web/PetController.java
+++ src/main/java/com/ashu/demo/web/PetController.java
@@ -12,19 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ashu.demo.model.Pet;
 import com.ashu.demo.repository.PetRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping(value = "/pets", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
+@RequiredArgsConstructor
 public class PetController {
 
 	private final PetRepository petRepository;
-
-	public PetController(final PetRepository petRepository) {
-		super();
-		this.petRepository = petRepository;
-	}
 
 	@PostMapping
 	public void create(@RequestBody Pet pet) {
